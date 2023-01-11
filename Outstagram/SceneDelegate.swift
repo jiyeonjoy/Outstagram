e28bee13cc8d6ad1d596e84f36ba8f00187bd150
최지연 <choi02647@naver.com>
2023-01-11 13:44:44 +0900

    added window tint color

--- Outstagram/SceneDelegate.swift
+++ Outstagram/SceneDelegate.swift
@@ -16,6 +16,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         window = UIWindow(windowScene: windowScene)
         window?.backgroundColor = .systemBackground
         window?.rootViewController = UINavigationController(rootViewController: TabBarController())
+        window?.tintColor = .label
         window?.makeKeyAndVisible()
     }
 }
